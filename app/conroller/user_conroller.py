4c8c131f5da82033386e5d3425963d75df051edb
emmanuel tindi <emmanueltindi66@gmail.com>
2024-12-27 19:16:16 +0300

    register

--- app/conroller/user_conroller.py
+++ app/conroller/user_conroller.py
@@ -1,12 +1,31 @@
 from app.db import db
+import bcrypt
 from app.models.user import User
+from flask import jsonify
+
 
 class UserContoller:
     def create_user(self, name, email, password):
-        db.session.add(User(name, email, password))
+        user = db.session.query(User).filter_by(email=email).first()
+        if user:
+            return jsonify({"error": "user already exists"}), 400
+        hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        db.session.add(User(name, email, password=hash))
         db.session.commit()
+        return jsonify({"message": "User created succesfully"}), 200
+
 
     def authenticate_user(self, email, password):
-        pass
+        user = db.session.query(User).filter_by(email=email).first()
+        if not user:
+            return jsonify({"error": "User not found check your email"}), 404
+        if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            return jsonify({"error": "Password does not match"}), 400
+            
+        
+        return jsonify({"Message": "Login successful"}), 200
+    
+
 
 user_controller = UserContoller()
